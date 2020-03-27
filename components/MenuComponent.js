ab14ce600170257e626c750a81ec3cabace7a353
osamahabib5 <osama.habib@khi.iba.edu.pk>
2020-03-27 17:55:00 +0500

    First commit 27 March 2020

---
+++ components/MenuComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+function Menu(props) {
+    const renderMenuItem = ({ item, index }) => {
+        return (
+            <ListItem
+                key={index}
+                title={item.name}
+                subtitle={item.description}
+                hideChevron={true}
+                leftAvatar={{ source: require('./images/uthappizza.png') }}
+            />
+        )
+    }
+
+    return (
+        <FlatList
+            data={props.dishes}    //props.dishes i
+            renderItem={renderMenuItem}  //renderItem: how to render each item in the list,
+            //this takes a parameter which renders each item at the list
+            //props.dishes are an object and FLatlist would iterate over each of them
+            //and render each given on the view in renderItem
+            keyExtractor={item => item.id.toString()} //keyExtractor:
+        //extract one of props of each item and use it as a key
+        />
+
+    )
+}
+
+export default Menu;
