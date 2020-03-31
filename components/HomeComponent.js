094f7a41e566b5164bbc50bc4c546cc062213435
osamahabib5 <osama.habib@khi.iba.edu.pk>
2020-03-31 18:08:28 +0500

    react navigation part 3

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+function RenderItem(props) {
+    const item = props.item;
+    if (item != null) {
+        return (
+            <Card
+                featuredTitle={item.name}
+                featuredSubtitle={item.designation}
+                image={require('./images/uthappizza.png')}
+            >
+                <Text style={{ margin: 10 }}>
+                    {item.description}
+                </Text>
+            </Card>
+        )
+    }
+    else {
+        return (
+            <View></View>
+        )
+    }
+}
+
 
 //created home component to use drawer based menu
 class Home extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        }
+    }
     render() {
         return (
-            <View><Text>Home Component</Text></View>
+            <ScrollView>
+                <RenderItem
+                    //filter method would only bring the dishes whose featured is true and only the first element
+                    item={this.state.dishes.filter((dish) => dish.featured)[0]} />
+                <RenderItem
+                    //filter method would only bring the dishes whose featured is true and only the first element
+                    item={this.state.promotions.filter((promo) => promo.featured)[0]} />
+                <RenderItem
+                    //filter method would only bring the dishes whose featured is true and only the first element
+                    item={this.state.leaders.filter((leader) => leader.featured)[0]} />
+            </ScrollView>
         )
     }
 }
