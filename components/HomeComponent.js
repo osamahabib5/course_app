6c556c486686dea9959aa1a074e17758d588d92c
osamahabib5 <osama.habib@khi.iba.edu.pk>
2020-03-31 15:11:30 +0500

    React Navigation Part 2

---
+++ components/HomeComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+//created home component to use drawer based menu
+class Home extends Component {
+
+    render() {
+        return (
+            <View><Text>Home Component</Text></View>
+        )
+    }
+}
+
+export default Home;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,45 +1,70 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { View, Platform } from 'react-native';
-import { DISHES } from '../shared/dishes';
+import Home from './HomeComponent';
 import Dishdetail from './DishDetailComponent';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 //Stack will use menu component and dishdetail component
 //and setup navigation between them
 //supply configuration for the 2 navigations 
 const Stack = createStackNavigator();
+const Drawer = createStackNavigator();
+//creating object for createDrawerNavigator
+const Drawer_Creat = createDrawerNavigator();
 
-class MyStack extends Component {
-    render() {
-        return (
-            <Stack.Navigator
-                initialRouteName="Menu"  //Stack Navigator starts with menu as the first screen when this component
-                //is the stack
-                headerMode="screen"
-                screenOptions={{     //screen options will be applied to all screens in the stack navigator Component
-                    headerStyle: { backgroundColor: '#512DA8' },  //background color for stack
-                    headerTintColor: '#fff',   //used for any icons used in headerstyle
-
-                }} >
-                <Stack.Screen name="Menu" component={Menu} />
-                <Stack.Screen name="Dishdetail" component={Dishdetail} />
-            </Stack.Navigator>
-
-        );
-    }
-
+//drawer based component
+function MainNavigator() {
+    return (
+        <Drawer_Creat.Navigator>
+            <Drawer_Creat.Screen name="Home" component={Home_Navigator} />
+            <Drawer_Creat.Screen name="Menu" component={MyStack} />
+        </Drawer_Creat.Navigator>
+    )
+}
+function MyStack() {
+    return (
+        <Stack.Navigator
+            initialRouteName="Menu"  //Stack Navigator starts with menu as the first screen when this component
+            //is the stack
+            headerMode="screen"
+            screenOptions={{     //screen options will be applied to all screens in the stack navigator Component
+                headerStyle: { backgroundColor: '#512DA8' },  //background color for stack
+                headerTintColor: '#fff',   //used for any icons used in headerstyle
+                headerTitleStyle: {
+                    color: '#ffff'
+                }
+            }} >
+            <Stack.Screen name="Menu" component={Menu} />
+            <Stack.Screen name="Dishdetail" component={Dishdetail} />
+        </Stack.Navigator>);
+}
+//we created Home_Navigator because createStackNavigator proides the status bar, a way of specifying
+//the navigation and title for the home
+//if we dont use createStackNavigator we dont get access to that
+function Home_Navigator() {
+    return (
+        <Drawer.Navigator
+            headerMode="screen"
+            screenOptions={{     //screen options will be applied to all screens in the stack navigator Component
+                headerStyle: { backgroundColor: '#512DA8' },  //background color for stack
+                headerTintColor: '#fff',   //used for any icons used in headerstyle
+                headerTitleStyle: {
+                    color: '#ffff'
+                }
+            }} >
+            <Drawer.Screen name="Home" component={Home} />
+        </Drawer.Navigator>)
 }
 class Main extends Component {
 
     render() {
         return (
-            <View style={
-                {
-                    paddingTop: 1,//Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,  //Platform.OS returns the platform running
-                    flex: 1
-                }
-            }>
+            <View style={{
+                paddingTop: 1,//Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,  //Platform.OS returns the platform running
+                flex: 1
+            }}>
                 {/* <Menu dishes={this.state.dishes}
                     onPress={(dishid) => this.onDishSelect(dishid)}      //selecting a dish where all dishes 
                 //are rendered
@@ -50,7 +75,7 @@ class Main extends Component {
                 filter return the subarray meeting the criteria where dishid is same as selected dish */}
 
                 {/* using stack function */}
-                <MyStack />
+                <MainNavigator />
             </View>
         )
     }
